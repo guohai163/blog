f8264f556a0e1a857149390d3ce9fce566d191e0
guohai <guohai@gmail.com>
2014-06-18 23:32:58 +0800

    update mybatis

--- blog/src/main/java/org/guohai/blog/ContactController.java
+++ blog/src/main/java/org/guohai/blog/ContactController.java
@@ -1,5 +1,9 @@
 package org.guohai.blog;
 
+import java.util.List;
+
+import org.guohai.blog.dao.ContactDAO;
+import org.guohai.blog.model.Contact;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -16,9 +20,18 @@ public class ContactController {
 	 */
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+
+	private static ContactDAO contactDAO;
+	
 	@RequestMapping(value="/blogid/{blogid}")
 	public String GetContactFromCode(@PathVariable("blogid") String blogid, Model model){
 		logger.info("blogid is :"+blogid);
+		
+
+		contactDAO= new ContactDAO();
+		
+		List<Contact> list = contactDAO.selectAll();
+		
 		return "contact";
 	}
 }
