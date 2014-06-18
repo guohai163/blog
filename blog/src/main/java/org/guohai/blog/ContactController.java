18dfe77cd7377d40e51eb3c32229e8732e70287c
guohai <guohai@gmail.com>
2014-06-18 17:26:26 +0800

    add mybatis velocity config

---
+++ blog/src/main/java/org/guohai/blog/ContactController.java
@@ -0,0 +1,24 @@
+package org.guohai.blog;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/contact")
+public class ContactController {
+
+	/**
+	 * loger
+	 */
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value="/blogid/{blogid}")
+	public String GetContactFromCode(@PathVariable("blogid") String blogid, Model model){
+		
+		return "content";
+	}
+}
---
+++ blog/src/main/java/org/guohai/blog/model/Contact.java
@@ -0,0 +1,17 @@
+package org.guohai.blog.model;
+
+public class Contact {
+	
+	private int code;
+	private String title;
+	private String intro;
+	
+	public Contact(int code,String title,String intro){
+		super();
+		this.code=code;
+		this.title=title;
+		this.intro=intro;
+	}
+
+	public Contact(){}
+}
