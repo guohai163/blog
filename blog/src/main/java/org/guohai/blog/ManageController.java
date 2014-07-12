3fe91d36a9c7f4c113f10bc3255f37b72d2e1c15
unknown <guohai@gmail.com>
2014-07-13 00:14:41 +0800

    add post method
    About manage page.add post function.

---
+++ blog/src/main/java/org/guohai/blog/ManageController.java
@@ -0,0 +1,31 @@
+package org.guohai.blog;
+
+import java.util.Locale;
+
+import org.guohai.blog.model.Contact;
+import org.json.JSONArray;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class ManageController {
+	
+	private static final Logger logger = LoggerFactory
+			.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/add-contact/", method = RequestMethod.POST)
+	public String AddContact(@RequestBody String requestBody) {
+		logger.info(requestBody);
+		Contact contact = new Contact();
+		
+		
+		return "manage/add-contact";
+	}
+}
