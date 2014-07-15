ed3753ce100ba82f1807612e7a7570485a52b648
unknown <guohai@gmail.com>
2014-07-16 00:17:39 +0800

    .......

--- blog/src/main/java/org/guohai/blog/ManageController.java
+++ blog/src/main/java/org/guohai/blog/ManageController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.guohai.blog.bll.ContactBLL;
 import org.guohai.blog.model.Contact;
+import org.guohai.blog.model.PostContact;
 import org.json.JSONArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,25 +14,27 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
-@RequestMapping("/manage")
+@RequestMapping(value="/manage")
 public class ManageController {
 	
 	private static final Logger logger = LoggerFactory
 			.getLogger(HomeController.class);
 	
-	@RequestMapping(value = "/add-contact", method = RequestMethod.GET)
-	public String AddContact(@RequestBody Contact contact) {
-		logger.info(contact.getTitle());
+	
+	@RequestMapping(value = "/add-contact", method = RequestMethod.POST)
+	public String AddContact(@RequestBody PostContact contact) {
+		logger.info(contact.title);
 		//Contact contact = new Contact();
 		//ContactBLL  contactBll = new ContactBLL();
 		//contactBll.AddPost();
 		return "manage/add-contact";
 	}
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String AddContact(){
 		return "manage/add-contact";
 	}
---
+++ blog/src/main/java/org/guohai/blog/model/PostContact.java
@@ -0,0 +1,6 @@
+package org.guohai.blog.model;
+
+public class PostContact {
+	public  String title;
+	public  String smallTitle;
+}
